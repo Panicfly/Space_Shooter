ad13fd59d145e65a3c765434d6d722a8cdb0fb4a
Panicfly <37988989+Panicfly@users.noreply.github.com>
2023-12-03 02:20:47 +0100

    Add Game Over Screen & Ship Up and Down Borders

--- ship.gd
+++ ship.gd
@@ -2,12 +2,17 @@ extends Area2D
 
 const LASER_SCENE = preload("res://laser.tscn")
 @export var speed = 100
+var height = ProjectSettings.get_setting("display/window/size/viewport_height") - 8.5
+
+signal ship_destroyed
 
 func _process(delta):
 	if Input.is_action_pressed("ui_up"):
-		position.y -= speed * delta
+		if position.y >= 8.5:
+			position.y -= speed * delta
 	if Input.is_action_pressed("ui_down"):
-		position.y += speed * delta
+		if position.y <= height:
+			position.y += speed * delta
 	if Input.is_action_just_pressed("ui_accept"):
 		var laser = LASER_SCENE.instantiate()
 		var main = get_tree().current_scene
@@ -17,3 +22,4 @@ func _process(delta):
 func _on_area_entered(area):
 	queue_free()
 	area.queue_free()
+	ship_destroyed.emit()
