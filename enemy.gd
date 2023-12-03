90a971aca720d55630fb6543ddfcd27f04346ba4
Panicfly <37988989+Panicfly@users.noreply.github.com>
2023-12-03 03:12:38 +0100

    Add Highscore System with Saves and Loads + Punishment for passing enemies

--- enemy.gd
+++ enemy.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
-@export var speed = 20
-@export var armor = 3
+@export var speed = 40
+@export var armor = 4
 
 func _process(delta):
 	position.x -= speed * delta
@@ -11,8 +11,10 @@ func _on_body_entered(body):
 	armor -= 1
 	if armor <= 0: 
 		var world = get_tree().current_scene
-		world.score += 1
+		world.score += 100
 		queue_free()
 
 func _on_visible_on_screen_enabler_2d_screen_exited():
+	var world = get_tree().current_scene
+	world.hearts -= 1
 	queue_free()
--- save_and_load.gd
+++ save_and_load.gd
@@ -1,11 +1,16 @@
 extends Node
 
+const SAVE_PATH = "res://save.cfg"
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func save_highscore(highscore):
+	var config = ConfigFile.new()
+	config.set_value("Game", "Highscore", highscore)
+	config.save(SAVE_PATH)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func load_highscore():
+	var highscore = 0
+	var config = ConfigFile.new()
+	var error = config.load(SAVE_PATH)
+	if error != OK: return null
+	highscore = config.get_value("Game", "Highscore")
+	return highscore
